e4eed2fb2556e895899cefa470236da81522f37b
Miles Dobrenski <milesdobrenski@gmail.com>
2023-11-22 20:05:04 -0800

    Creating homescreen, updating logscreen

---
+++ iosdecal/iosdecalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosdecalApp.swift
+//  iosdecal
+//
+//  Created by Miles Dobrenski on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct iosdecalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogView()
+        }
+    }
+}
