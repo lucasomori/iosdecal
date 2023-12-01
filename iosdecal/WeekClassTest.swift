a338ae367996d644269467e3e5d13168a64de806
David <davidspitz900@gmail.com>
2023-11-30 21:43:00 -0800

    Added "singleton" pattern for WeekClass objects

--- iosdecal/WeekClassTest.swift
+++ iosdecal/WeekClassTest.swift
@@ -11,16 +11,22 @@ import SwiftData
 struct WeekClassTest: View {
     @Environment(\.modelContext) var context
     //@State private var week = WeekClass()
-    
+    @Query var checkWeeks: [WeekClass]
     var body: some View {
         VStack {
             Text("Hello world!")
             Button(action: {
-                    context.insert(WeekClass())
+                CheckWeeks()
             }) {
-                Text("IDK")
+                Text("\(checkWeeks.count)")
             }
             WeekClassTest2()
+        }
+    }
+    
+    func CheckWeeks() {
+        if (checkWeeks.count < 1) {
+            context.insert(WeekClass())
         }
     }
 }
@@ -28,9 +34,12 @@ struct WeekClassTest: View {
 struct WeekClassTest2: View {
     @Query var currWeek: [WeekClass]
     var body: some View {
-        let score = currWeek[0].GetWeekScore()
-        Text("Score: \(score)")
-        //Text("Hello world!")
+        if (currWeek.count > 0) {
+            let score = currWeek[0].GetWeekScore()
+            Text("Score: \(score)")
+            Text("Count: \(currWeek.count)")
+            //Text("Hello world!")
+        }
     }
 }
 #Preview {
